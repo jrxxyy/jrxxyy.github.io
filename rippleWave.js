5b07d838e0153f82d60b1527a86e95e90e6ee9a3
jrxxyy <146299622+jrxxyy@users.noreply.github.com>
2025-02-08 19:04:16 -0800

    Update rippleWave.js

--- rippleWave.js
+++ rippleWave.js
@@ -16,12 +16,13 @@
                 const height = canvas.height;
                 const img = new Image();
                 
+                img.crossOrigin = "Anonymous"; // Allow cross-origin image usage
                 img.src = 'https://www.bing.com/th/id/OGC.44ec170cc5530364edce8f175251690d?pid=1.7&rurl=https%3a%2f%2fwww.designcoding.net%2fdecoder%2fwp-content%2fuploads%2f2023%2f04%2f2022_10_04-fourier.gif&ehk=ZLB2euoqD%2bT7b9AR1%2fajNC9LDda%2fensf0g9uEngQJeA%3d'; // Direct URL to your image
 
                 img.onload = function() {
                     function drawWave() {
                         ctx.clearRect(0, 0, width, height);
-                        ctx.drawImage(img, 0, 0, width, height);
+                        ctx.drawImage(img, 0, 0, width, height); // Draw the image first
                         ctx.beginPath();
                         for (let y = 0; y < height; y++) {
                             const waveValue = amplitude * Math.sin(frequency * y);
